b35ccefe206c36bf2427eb8d745be3206d36b303
Andriikryl <andreyk557@gmail.com>
2024-02-04 17:10:04 +0200

    add styles for header and footer component

--- src/components/Counte.svelte
+++ src/components/Counte.svelte
@@ -1,16 +1,30 @@
 <script>
-  let count = 0;
-  function dicriment() {
-    count = count + 1;
+  let valueNumber = "";
+  let list = [];
+  function addToList() {
+    list = [...list, valueNumber];
+    valueNumber = "";
   }
 </script>
 
 <section class="counter">
   <div class="container">
     <div>
-      <h3>counter</h3>
-      <h2>{count}</h2>
-      <button on:click={dicriment}>dincrement</button>
+      <p>{valueNumber}</p>
+      <form on:submit|preventDefault>
+        <div>
+          <label for="input">type your number heare</label>
+          <input bind:value={valueNumber} type="number" />
+        </div>
+        <button on:click={addToList}>add number</button>
+      </form>
+      <div>
+        <ol>
+          {#each list as item}
+            <li>{item}</li>
+          {/each}
+        </ol>
+      </div>
     </div>
   </div>
 </section>
