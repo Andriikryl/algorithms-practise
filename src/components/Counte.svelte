0ace04feab9ff4f45c16a6d2971a0aca482b2f60
Andriikryl <andreyk557@gmail.com>
2024-02-04 16:45:13 +0200

    add counter component

---
+++ src/components/Counte.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0;
+  function dicriment() {
+    count = count + 1;
+  }
+</script>
+
+<section class="counter">
+  <div class="container">
+    <div>
+      <h3>counter</h3>
+      <h2>{count}</h2>
+      <button on:click={dicriment}>dincrement</button>
+    </div>
+  </div>
+</section>
